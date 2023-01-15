760b1de2c9c3c25e7a5e827e127667e83e09a2f7
KesterDaniel <kesterdan17@gmail.com>
2023-01-15 09:47:55 +0100

    final commit

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ coffee = CoffeeMaker()
 menu = Menu()
 payment = MoneyMachine()
 
+
 machine_on = True
 
 while machine_on:
     user_input = input("What would you like to order? espresso/latte/cappuccino: ")
     if user_input == "report":
         coffee.report()
+        payment.report()
     elif user_input == "off":
         machine_on = False
     else:
@@ -21,7 +23,12 @@ while machine_on:
         if order_item is not None:
             drink_available = coffee.is_resource_sufficient(order_item)
             if drink_available:
-                payment = round(payment.process_coins(), 2)
-                print(payment)
+                item_cost = order_item.cost
+                payment_success = payment.make_payment(item_cost)
+                if payment_success:
+                    coffee.make_coffee(order_item)
+
+
+
 
 
