313e7481cf432463a99614c75f9ef71fae65bdc4
KesterDaniel <kesterdan17@gmail.com>
2023-01-15 08:19:45 +0100

    print report of available resources based on user input

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+
+machine_on = True
+
+while machine_on:
+    user_input = input("What would you like to order? espresso/latte/cappuccino: ")
+    if user_input == "report":
+        CoffeeMaker.report()
