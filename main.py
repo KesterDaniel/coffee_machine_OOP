25997f6e61b5dbdacdd72f6dcdf9a9280c48fdd9
KesterDaniel <kesterdan17@gmail.com>
2023-01-15 08:51:46 +0100

    find drink based on user input

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+coffee = CoffeeMaker()
+menu = Menu()
 
 machine_on = True
 
 while machine_on:
     user_input = input("What would you like to order? espresso/latte/cappuccino: ")
     if user_input == "report":
-        CoffeeMaker.report()
+        coffee.report()
+    elif user_input == "off":
+        machine_on = False
+    else:
+        user_order = menu.find_drink(user_input)
+        
